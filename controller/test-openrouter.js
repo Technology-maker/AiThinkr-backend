ea7883dc6b64ae436f1d6f1dc2c08f17a1daba99
Technology-maker <ys5401519@gmail.com>
2025-08-07 17:31:13 +0530

    changing api

--- controller/test-openrouter.js
+++ controller/test-openrouter.js
@@ -5,7 +5,7 @@ dotenv.config();
 const openai = new OpenAI({
   baseURL: "https://openrouter.ai/api/v1",
   defaultHeaders: {
-    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY || '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'}`,
   },
 });
 
